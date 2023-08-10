d49096c3488a411052292736c9fb164adfff6542
Alyssa <alyssa.brooksong@gmail.com>
2023-08-10 16:36:16 -0700

    CLI

--- lib/db/seed.py
+++ lib/db/seed.py
@@ -8,6 +8,9 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
+    session.query(Character).delete()
+    session.query(Scene).delete()
+
     # Character------------------------------------
 
     Albert = Character(name="Albert", description="Pig, straw house")
@@ -58,7 +61,7 @@ if __name__ == "__main__":
     )
     scene_8 = Scene(
     scene_num=8, 
-    description="It's your neighbor THE Wolf. He introduces himself 'Hello neighbor, I'm Theodore Harold Edgar Wolf. So sorry to bother you but could you spare some hot tea? I seem to find myself with a cold and AAACHOOOO!' He sneezes a great sneeze and destroys your brand new house! It scares you so you run to your sibling\'s house. But you have two. Which one will you run to?", 
+    description="It's your neighbor THE Wolf. He introduces himself 'Hello neighbor, I'm Theodore Harold Edgar Wolf. So sorry to bother you but could you spare some hot tea? I seem to find myself with a cold AAACHOOOO!' He sneezes a great sneeze and destroys your brand new house! It scares you and you run to your sibling\'s house. But you have two. Which one will you run to?", 
     choice_A="other lazy sibling", 
     choice_A_next_scene=11, 
     choice_B="Charlie's", 
@@ -73,7 +76,7 @@ if __name__ == "__main__":
     )
     scene_10 = Scene(
         scene_num=10, 
-    description="You say 'No! No! No! Not by hairs on my chinny chin chin!' BB Wolf shows his teeth and says, 'I am Mr. Big Bad Wolf! Let me in or I'll HUFF and I'll PUFF and I'll blow your house down.' So he huffs and puffs and the blows the house down! He goes to bite you but you escape and run to your siblings house. But you have two. Which one will you run to?", 
+    description="You say 'No! No! No! Not by hairs on my chinny chin chin!' BB Wolf shows his teeth and says, 'I am Mr. Big Bad Wolf! Let me in or I'll HUFF and I'll PUFF and I'll blow your house in.' So he huffs and puffs and the blows the house in! He goes to bite you but you escape and run to your siblings house. But you have two. Which one will you run to?", 
     choice_A="other lazy sibling", 
     choice_A_next_scene=13, 
     choice_B="Charlie's", 
@@ -91,14 +94,14 @@ if __name__ == "__main__":
         )
     scene_12 = Scene(
         scene_num=12, 
-        description="It's your neighbor THE Wolf. He introduces himself 'Hello neighbor, I'm Theodore Harold Edgar Wolf. So sorry to bother you but could you spare some hot tea? I seem to find myself with a cold and AAACHOOOO!' He sneezes a great sneeze and destroys your brand new house! It scares you both so much so you run to your Charlie's house.", 
+        description="It's your neighbor THE Wolf. He introduces himself 'Hello neighbor, I'm Theodore Harold Edgar Wolf. So sorry to bother you but could you spare some hot tea? I seem to find myself with a cold AAACHOOOO!' He sneezes a great sneeze and destroys your brand new house! It scares you both so much so you run to Charlie's house.", 
         choice_A="go to Charlie's", 
         choice_A_next_scene=15, 
         wolf="THE Wolf"
         )
     scene_13 = Scene(
         scene_num=13, 
-        description="You get to their house and run inside. Minutes later there's a knock at your door. It's your neighbor BB Wolf. He introduces himself 'Hello neighbor, I'm Mr. BB Wolf. Little pig! Little pig! Let me in! Let me in!' Do you:", 
+        description="You get to their house and run inside. Minutes later there's a knock at the door. It's your neighbor BB Wolf. He introduces himself 'Hello neighbor, I'm Mr. BB Wolf. Little pig! Little pig! Let me in! Let me in!' Do you:", 
         choice_A="open door", 
         choice_A_next_scene=9, 
         choice_B="refuse", 
@@ -107,7 +110,7 @@ if __name__ == "__main__":
         )
     scene_14 = Scene(
         scene_num=14, 
-        description="You say 'No! No! No! Not by hairs on my chinny chin chin!' BB Wolf shows his teeth and says, 'I am the Big Bad Wolf! Let me in or I'll HUFF and I'll PUFF and I'll blow your house down.' So he huffs and puffs and the blows the house down! He goes to bite you but you escape and run to your Charlie's house.", 
+        description="You say 'No! No! No! Not by hairs on my chinny chin chin!' BB Wolf shows his teeth and says, 'I am the Big Bad Wolf! Let me in or I'll HUFF and I'll PUFF and I'll blow your house in.' So he huffs and puffs and the blows the house in! He goes to bite you but you escape and run to your Charlie's house.", 
         choice_A="go to Charlie's", 
         choice_A_next_scene=15, 
         wolf="BB Wolf"
@@ -124,7 +127,7 @@ if __name__ == "__main__":
     scene_16 = Scene(
         scene_num=16, 
         description="At Charlie's house, there's a knock at your door.  It's your neighbor BB Wolf. He introduces himself 'Hello neighbor, I'm BB Wolf. Little pig! Little pig! Let me in! Let me in!' Do you:", 
-        choice_A="ope door", 
+        choice_A="open door", 
         choice_A_next_scene=9, 
         choice_B="refuse", 
         choice_B_next_scene=18, 
@@ -132,13 +135,13 @@ if __name__ == "__main__":
         )
     scene_17 = Scene(
         scene_num=17, 
-        description="It's your neighbor THE Wolf. He introduces himself 'Hello neighbor, I'm Theodore Harold Edgar Wolf. So sorry to bother you but could you spare some hot tea? I seem to find myself with a cold and AAACHOOOO!' He sneezes a great sneeze. Nothing happens. You peek your head out the door and you invite the wolf in for tea and end up talking. He\'s actually vegetarin and doesn\'t eat meat. You now have a friend for life. THE END", 
+        description="It's your neighbor THE Wolf. He introduces himself 'Hello neighbor, I'm Theodore Harold Edgar Wolf. So sorry to bother you but could you spare some hot tea? I seem to find myself with a cold AAACHOOOO!' He sneezes a great sneeze. Nothing happens. You peek your head out the door and you invite the wolf in for tea and end up talking. He\'s actually vegetarin and doesn\'t eat meat. You now have a friend for life. THE END", 
         choice_A="return to main menu", 
         wolf="THE Wolf"
         )
     scene_18 = Scene(    
         scene_num=18, 
-        description="You say 'No! No! No! Not by hairs on my chinny chin chin!' BB Wolf shows his teeth and says, 'I am Mr. Big Bad Wolf! Let me in or I'll HUFF and I'll PUFF and I'll blow your house down.' So he huffs and puffs and nothing happens. He keeps huffing and puffing but he can't blow the house down. Finally he'so out of breath that he can't huff and puff anymore. So he stopped to rest. He decides to climb onto the roof and finally get these pigs for good. You hear him climbing onto the roof. Do you: ", 
+        description="You say 'No! No! No! Not by hairs on my chinny chin chin!' BB Wolf shows his teeth and says, 'I am Mr. Big Bad Wolf! Let me in or I'll HUFF and I'll PUFF and I'll blow your house in.' So he huffs and puffs and nothing happens. He keeps huffing and puffing but he can't blow the house in. Finally he'so out of breath that he can't huff and puff anymore. So he stopped to rest. He decides to climb onto the roof and finally get these pigs for good. You hear him climbing onto the roof. Do you: ", 
         choice_A="make a blazing fire in your fireplace under a big pot of water", 
         choice_A_next_scene=19, 
         choice_B="don't make a fire", 
@@ -147,7 +150,7 @@ if __name__ == "__main__":
         )
     scene_19 = Scene(
         scene_num=19, 
-        description="BB Wolf comes down the chimney and plop! he falls into the boiling water. You slam the lid on. And boil the wolf up and you and your siblings have a have wolf stew for dinner. THE END", 
+        description="BB Wolf comes down the chimney and plop! He falls into the boiling water. You slam the lid on. And boil the wolf up and you and your siblings have a have wolf stew for dinner. THE END", 
         choice_A="return to main menu", 
         wolf="BB Wolf"
         )
@@ -160,3 +163,5 @@ if __name__ == "__main__":
     
     session.add_all(scenes)
     session.commit()
+
+    
